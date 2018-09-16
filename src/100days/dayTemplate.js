6db5b52b2295441e92d2b22fe051d3f8bdbf9fa6
NiKole Maxwell <nvymaxwell@outlook.com>
2018-09-15 22:15:50 -0400

    commit it

--- src/100days/dayTemplate.js
+++ src/100days/dayTemplate.js
@@ -2,13 +2,40 @@
 /* Use this template each day to create a new entry for the 100 Days of Code */
 let dayTemplate = 
     {
-        "dayAvatarPath" : "path",
         "dayCount" : 00,
         "dayDate" : "Date",
+        "dayAvatarPath" : "path",
         "daySubTitle" : "subTitle",
         "dayType" :  ["project", "tutorial", "course"],
         "daySource" :  ["FreeCodeCamp", "YouTube", "Traversy","FlatIron", "Udacity" ],
         "dayTechUsed" : "tech list",
         "daysDescription" : "description",
-        "daysCode" : "daysCode"
+        "daysCode" : "daysCode",
+        "daysHash" : "daysHash"
     }
+
+
+                                                    function customerCareGreeting(type) {
+                                                        var message = "";
+
+                                                        // Return the following message based on the service type 
+
+                                                        switch (type){
+                                                            case "new":
+                                                            message = "Thank you for enrolling for our services";
+                                                            break;
+
+                                                            case "repeat":
+                                                            case "upgrade":
+                                                                message = "Thank you for your continued loyalty.";
+                                                                break;
+
+                                                            default: 
+                                                                "We appreciate  your business, thank you for choosing us.";
+                                                                break;
+                                                        }
+                                                            return message;
+                                                    }    
+                                                    
+                                                    customerCareGreeting("upgrade");
+                                                   // "Thank you for your continued loyalty."
--- src/scripts/dayslog.js
+++ src/scripts/dayslog.js
@@ -1,39 +1,25 @@
 /*For each json entry in the array I want to fill in innerHTML of the elements from the Object */
 
-//DEFINE Object Defaults
-let dayAvatarPath = "path";
-let dayCount = 00;
-let dayDate = Date.toLocaleString(Date);
-let daySubTitle = "subTitle"
-let dayType =  ["project", "tutorial", "course"];
-let daySource =  ["FreeCodeCamp", "YouTube", "Traversy","FlatIron", "Udacity" ];
-let dayTechUsed = "tech list";
-let daysDescription = "description";
-let daysCode = "daysCode";
-let daysHash = "daysHash";
-
 
 //Create Object
-let entry =  {
-    dayAvatarPath,
-    dayCount,
-    dayDate,
-    daySubTitle, 
-    dayType,
-    daySource,
-    dayTechUsed ,
-    daysDescription,
-    daysCode,
-    daysHash
-    };
-
-const entry01 = Object.create(entry);
+let entry = 
+    {
+        dayAvatarPath : "path",
+        dayCount : 00,
+        dayDate: "Date",
+        daySubTitle : "subTitle",
+        dayType :  ["project", "tutorial", "course"],
+        daySource :  ["FreeCodeCamp", "YouTube", "Traversy","FlatIron", "Udacity" ],
+        dayTechUsed: "tech list",
+        daysDescription : "description",
+        daysCode : "daysCode",
+        daysHash : "daysHash"
+    }
 
 //CREATE JSON ENTRY ---manual?
          /* --> Use dayTemplate.js. reserver chronological order */
 
 //GET JSON
-let rawJSON = "/100days/100DaysLog.json";
 
 // Parse JSON
         /* for each entry in the JSON, then do this insert 
